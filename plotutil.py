9e8d8f066fef0243f623f24edf45ae689fe9ecbf
jarmeister99 <jarmeister99@gmail.com>
2020-06-18 18:15:11 -0700

    fixed plot filling y location

--- plotutil.py
+++ plotutil.py
@@ -11,7 +11,7 @@ def add_head_and_tail(points, timestep, min, max):
     while min + (i * timestep) <= max:
         candidate_x = min + (i * timestep)
         if abs(new_points[i][0] - candidate_x) > FLOAT_EPSILON:
-            new_points.insert(i, (candidate_x, 1))
+            new_points.insert(i, (candidate_x, 0))
         else:
             i += 1
     return new_points
