d69bf74f2ea49cbb5f32812dabdb9fc15fb2add1
jarmeister <jarmeister99@gmail.com>
2020-06-21 10:52:57 -0700

    increased add_head_and_tail performance

--- plotutil.py
+++ plotutil.py
@@ -10,14 +10,10 @@ def add_head_and_tail(points, timestep, minval, maxval, already_sorted=False):
         points.sort()
     head = []
     tail = []
-    i = 0
-    while minval + (i * timestep) < points[0][0]:
-        head.append((minval + (i * timestep), 0))
-        i += 1
-    i = 1
-    tail_start = round(points[-1][0] * 2.0) / 2.0
-    while tail_start + (i * timestep) < maxval + timestep:
-        tail.append((tail_start + (i * timestep), 0))
-        i += 1
-    points = head + points + tail
-    return points
+    head.append((minval, 0))
+    if points[0][0] - timestep > 0:
+        head.append((points[0][0] - timestep, 0))
+    if points[-1][0] + timestep < maxval:
+        tail.append((points[-1][0] + timestep, 0))
+    tail.append((maxval, 0))
+    return head + points + tail
